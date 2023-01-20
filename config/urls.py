2dbf661c15b8d10e3ef6e193907acfa850405446
ikuma <chemicalbondo@gmail.com>
2023-01-20 17:34:56 +0900

    日本語表記に変更

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('board.urls')),
+    path('signup/', include('signup.urls')),
+    path('login/', include('login.urls')),
 ]
