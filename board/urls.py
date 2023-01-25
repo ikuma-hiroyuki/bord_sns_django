968aedd63a796bdd2583262814fbe7f344c62d80
ikuma <chemicalbondo@gmail.com>
2023-01-25 17:50:28 +0900

    listアプリ関連追加

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from board.views import boardfunc
+from board.views import boardfunc, listfunc
 
 urlpatterns = [
     path('', boardfunc, name='board'),
+    path('list/', listfunc, name='list'),
 ]
