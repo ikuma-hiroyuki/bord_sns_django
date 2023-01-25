17096e01e965163a752b1beffe99e890e7aa8f8d
ikuma <chemicalbondo@gmail.com>
2023-01-25 17:50:15 +0900

    listアプリ関連追加

--- board/models.py
+++ board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    snsimages = models.ImageField(upload_to='images/', null=True, blank=True)
+    good = models.IntegerField(default=0)
+    read = models.IntegerField(default=0)
+    readtext = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
