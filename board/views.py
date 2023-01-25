40cdaba858ab47dc2e9101eed421aa52aab72a07
ikuma <chemicalbondo@gmail.com>
2023-01-25 17:50:19 +0900

    listアプリ関連追加

--- board/views.py
+++ board/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from board.models import BoardModel
 
 
 def boardfunc(request):
     return HttpResponse('bord')
+
+
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list': object_list})
