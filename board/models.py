d4f455eb2c77a01920733189e4cd7dcf107a7905
ikuma <chemicalbondo@gmail.com>
2023-01-26 17:27:09 +0900

    画像のアップロード先の設定

--- board/models.py
+++ board/models.py
@@ -5,7 +5,7 @@ class BoardModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=50)
-    snsimages = models.ImageField(upload_to='images/', null=True, blank=True)
+    snsimages = models.ImageField(upload_to='', null=True, blank=True)
     good = models.IntegerField(default=0)
     read = models.IntegerField(default=0)
     readtext = models.TextField(null=True, blank=True)
--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls.static import static
 from django.urls import path
+from django.conf import settings
 
 from board.views import boardfunc, listfunc
 
 urlpatterns = [
     path('', boardfunc, name='board'),
     path('list/', listfunc, name='list'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
