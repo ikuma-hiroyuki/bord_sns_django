145a73374db1618b42b5494b9fc225618f44b212
ikuma <chemicalbondo@gmail.com>
2023-01-27 17:37:42 +0900

    logout, detail, good, read追加

--- board/urls.py
+++ board/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls.static import static
 from django.urls import path
 from django.conf import settings
 
-from board.views import boardfunc, listfunc
+from board.views import boardfunc, detailfunc, goodfunc, listfunc, readfunc
 
 urlpatterns = [
     path('', boardfunc, name='board'),
+    path('detail/<int:pk>', detailfunc, name='detail'),
     path('list/', listfunc, name='list'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('good/<int:pk>', goodfunc, name='good'),
+    path('read/<int:pk>', readfunc, name='read'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from board.views import logoutfunc
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('board.urls')),
     path('signup/', include('signup.urls')),
-    path('login/', include('login.urls')),
+    path('login/', include('login.urls'), ),
+    path('logout/', logoutfunc, name='logout'),
+    path('detail/<int:pk>', include('board.urls')),
+    path('good/<int:pk>', include('board.urls')),
+    path('read/<int:pk>', include('board.urls')),
 ]
