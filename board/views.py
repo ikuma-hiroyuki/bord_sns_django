a1e0e94c3210847e6cae8286d6d17f9a6c8ed02d
ikuma <chemicalbondo@gmail.com>
2023-01-27 17:38:32 +0900

    logout, detail, good, readの追加

--- board/views.py
+++ board/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from board.models import BoardModel
 
@@ -8,6 +10,36 @@ def boardfunc(request):
     return HttpResponse('bord')
 
 
+@login_required
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list': object_list})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
+
+def detailfunc(request, pk):
+    object = get_object_or_404(BoardModel, pk=pk)
+    return render(request, 'detail.html', {'object': object})
+
+
+def goodfunc(request, pk):
+    post = get_object_or_404(BoardModel, pk=pk)
+    post.good += 1
+    post.save()
+    return redirect('list')
+
+
+def readfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    username = request.user.get_username()
+    if username in object.readtext:
+        return redirect('list')
+    else:
+        object.read += 1
+        object.readtext += username + '\n'
+        object.save()
+        return redirect('list')
